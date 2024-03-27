a6e488daf660110185d3ceb7cdf81058d3441ee0
wexenvagyok <120109536+wexenvagyok@users.noreply.github.com>
2024-03-27 08:45:36 +0100

    Add files via upload

---
+++ expedíció/radio.py
@@ -0,0 +1,30 @@
+import radioClass as rc
+
+f = open("veetel.txt")
+sorszam = 0
+tempSor = ""
+uzenetek = []
+
+for egySor in f:
+    if sorszam %2 == 0:
+        tempSor = egySor
+    else:
+        uzenetek.append(rc.Uzenet(tempSor, egySor))
+
+    sorszam += 1
+      
+f.close()
+
+print("2. feladat")
+print("Az első üzenet rögzítője: {}".format(uzenetek[0].amator))
+print("Az utolsó üzenet rögzítője: {}".format(uzenetek[-1].amator))
+
+print("\n3. feladat")
+for egyUzenet in uzenetek:
+    if egyUzenet.farkasKereso():
+        print("{}. nap {}. rádióamatőr".format(egyUzenet.nap,egyUzenet.amator))
+
+napok=[]
+tempNap=rc.Nap(1)
+for egyUzenet in uzenetek:
+    pass
--- expedíció/radioClass.py
+++ expedíció/radioClass.py
@@ -1,3 +1,23 @@
-class uzenet:
-    def __init__ (self,sor1,sor2):
+class Uzenet:
+    def __init__(self,sor1,sor2):
+        #"2 15"
+        #"1/0 #gy#domb##l fig###tu# f#i#s ho#a##dalyoz$$"
         self.nap = int(sor1.split(" ")[0])
+        self.amator = int(sor1.split(" ")[1])
+
+        self.uzenet = sor2
+
+    def farkasKereso(self):
+        return "farkas" in self.uzenet
+    
+        if "farkas" in self.uzenet:
+            return True
+        else:
+            return False
+class Nap:
+    def __init__(self, nap):
+        self.nap=nap
+        self.uzenetek=[]
+    
+    def hozzaad(self,uzenet):
+        self.uzenetek.append
